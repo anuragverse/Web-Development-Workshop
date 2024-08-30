8005de0a8ea057a6a41e4be7429b463f3c55432b
Anurag Sinha <144948409+anuragverse@users.noreply.github.com>
2024-08-30 11:18:25 +0530

    Update First Typescript.ts

--- First Typescript.ts
+++ First Typescript.ts
@@ -1,3 +1,16 @@
+/*Installation Steps
+Open Powershell as Admin
+check node.js ver. by "node -v"
+type command "Set-ExecutionPolicy ExecutionPolicy RemoteSigned"
+Select "Y"
+now type "npm install -g typescript" on VS Code terminal
+now run "tsc fileName.ts" 
+new file with .js will appear in the folder
+open that file
+run "node fileName.js" 
+Output will be shown in the terminal now */
+
+
 class Person{
 private firstName: string;
 private lastName: string;
